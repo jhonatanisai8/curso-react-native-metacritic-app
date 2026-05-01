19bac55c916d35816bb634d3cb6b8d29ce141ddd
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-01 17:59:14 -0500

    feat: agregando el scrool view

--- App.js
+++ App.js
@@ -1,58 +1,47 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
 import {
-  Button,
   Image,
   Pressable,
+  ScrollView,
   StyleSheet,
   Text,
-  TouchableHighlight,
   View,
 } from "react-native";
+import { getCharacters } from "./lib/metacritic";
 
-// importamos el codigo
 const icono = require("./assets/icon.png");
 export default function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    getCharacters().then((chars) => {
+      setCharacters(chars);
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
-      {/* <Image fadeDuration={1} source={icono} style={styles.image} /> */}
-      {/* <Image
-        source={{
-          uri: "https://i.pinimg.com/1200x/8b/f9/7c/8bf97c46f37c04605fa0d45be069d4d4.jpg",
-        }}
-        style={styles.image}
-      /> */}
       <StatusBar style="light" />
-      {/* <Text
-        style={{
-          color: "#fff",
-        }}
-      >
-        Tenemos aqui la app.
-      </Text> */}
-      {/* boton nativo de react native */}
-      {/* <Button title="Presioname" onPress={() => alert("Hola")} /> */}
-      {/* <TouchableHighlight underlayColor={"#09f"} onPress={() => alert("Hola")}>
-        <Text style={{ color: "#fff" }}>Presioname</Text>
-      </TouchableHighlight> */}
-      <Pressable
-        onPress={() => {}}
-        style={({ pressed }) => [
-          {
-            backgroundColor: pressed ? "#09f" : "rgb(204, 23, 84)",
-          },
-          styles.wrapperCustom,
-        ]}
-      >
-        {({ pressed }) => (
-          <Text
-            style={{
-              fontSize: pressed ? 32 : 16,
-            }}
-          >
-            {pressed ? "Presionado!" : "Presioname"}
-          </Text>
-        )}
-      </Pressable>
+      <ScrollView>
+        {characters.map((character) => (
+          <View key={character.id} style={styles.card}>
+            <Image source={{ uri: character.image }} style={styles.image} />
+            <Text style={styles.title}>{character.name}</Text>
+            {character.phrases && character.phrases.length > 0 && (
+              <View>
+                <Text style={styles.descripcion}>Frases típicas:</Text>
+                {character.phrases.map((phrase, index) => (
+                  <Text key={index} style={styles.descripcion}>
+                    "{phrase}"
+                  </Text>
+                ))}
+              </View>
+            )}
+            <Text style={styles.score}>{character.status}</Text>
+          </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
@@ -64,9 +53,32 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  card: {
+    borderStyle: "solid",
+    borderColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
+    marginBottom: 48,
+  },
   image: {
-    width: 215,
-    height: 294,
-    resizeMode: "contain",
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 10,
+  },
+  descripcion: {
+    fontSize: 16,
+    color: "#fff",
+  },
+  score: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "green",
+    marginTop: 10,
   },
 });
