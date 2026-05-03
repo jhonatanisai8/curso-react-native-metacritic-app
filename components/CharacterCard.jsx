ff8cc68cd3bb7d8fa276a7f98b4ff1c8a0a36120
jhonatansai <ojedasnachezjhonatan@gmail.com>
2026-05-02 21:39:21 -0500

    Feat: card para personaje

---
+++ components/CharacterCard.jsx
@@ -0,0 +1,59 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export default function CharacterCard({ character }) {
+  return (
+    <View style={styles.card}>
+      <Image source={{ uri: character.image }} style={styles.image} />
+      <Text style={styles.title}>{character.name}</Text>
+      {character.phrases && character.phrases.length > 0 && (
+        <View style={styles.frases}>
+          <Text>Frases típicas:</Text>
+          {character.phrases.map((phrase, index) => (
+            <Text style={styles.frase} key={index}>
+              "{phrase}"
+            </Text>
+          ))}
+        </View>
+      )}
+      <Text style={styles.score}>{character.status}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    borderStyle: "solid",
+    borderColor: "#fff",
+    borderRadius: 10,
+    borderWidth: 2,
+    marginBottom: 48,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+    marginBottom: 10,
+  },
+  frases: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#fff",
+    paddingRight: 5,
+    paddingLeft: 5,
+  },
+  frase: {
+    fontSize: 16,
+    color: "#04c059",
+  },
+  score: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#EAB464",
+    marginTop: 10,
+  },
+});
--- components/Main.jsx
+++ components/Main.jsx
@@ -11,6 +11,7 @@ import {
   View,
 } from "react-native";
 import { getCharacters } from "../lib/metacritic";
+import CharacterCard from "./CharacterCard";
 
 const icono = require("../assets/icon.png");
 export default function Main() {
@@ -32,61 +33,12 @@ export default function Main() {
     >
       <ScrollView>
         {characters.map((character) => (
-          <View key={character.id} style={styles.card}>
-            <Image source={{ uri: character.image }} style={styles.image} />
-            <Text style={styles.title}>{character.name}</Text>
-            {character.phrases && character.phrases.length > 0 && (
-              <View style={styles.frases}>
-                <Text>Frases típicas:</Text>
-                {character.phrases.map((phrase, index) => (
-                  <Text style={styles.frase} key={index}>
-                    "{phrase}"
-                  </Text>
-                ))}
-              </View>
-            )}
-            <Text style={styles.score}>{character.status}</Text>
-          </View>
+          <CharacterCard
+            key={character.id}
+            character={character}
+          ></CharacterCard>
         ))}
       </ScrollView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  card: {
-    borderStyle: "solid",
-    borderColor: "#fff",
-    borderRadius: 10,
-    borderWidth: 2,
-    marginBottom: 48,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: "#fff",
-    marginBottom: 10,
-  },
-  frases: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#fff",
-    paddingRight: 5,
-    paddingLeft: 5,
-  },
-  frase: {
-    fontSize: 16,
-    color: "#04c059",
-  },
-  score: {
-    fontSize: 18,
-    fontWeight: "bold",
-    color: "#EAB464",
-    marginTop: 10,
-  },
-});
